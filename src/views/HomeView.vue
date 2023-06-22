d491525c5a379ee187522caf697c892940df2371
DannielNavas <dannielnavas@gmail.com>
2023-06-22 06:59:46 -0500

    ✨ Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <script setup>
+import { ref } from 'vue';
+
+const username = ref('');
+
+
 </script>
 
 <template>
   <main>
-    Home
+    <h3>Introduce tu nombre de usuario {{ username }}</h3>
+    <input type="text" placeholder="username" v-model="username">
   </main>
 </template>
