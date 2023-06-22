69ec11fb79940df85831f5a973f9ec94f7ea4447
DannielNavas <dannielnavas@gmail.com>
2023-06-22 08:42:10 -0500

    ✨ maned views

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <div>
       <router-link :to="{name: 'about'}">About</router-link>
       <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link to="/session">Session</router-link>
       <router-link to="/chats">chats</router-link>
     </div>
   </div>
