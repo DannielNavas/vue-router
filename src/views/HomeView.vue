2965e23db0a0e25a9feec0534a92444cd539eb17
DannielNavas <dannielnavas@gmail.com>
2023-06-22 08:23:36 -0500

    ✨ useRouter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const username = ref('');
 
-
+const iniciar = () => {
+    router.push(
+        '/chats',
+    );
+};
 </script>
 
 <template>
   <main>
     <h3>Introduce tu nombre de usuario {{ username }}</h3>
     <input type="text" placeholder="username" v-model="username">
+    <button @click="iniciar">Iniciar sesion</button>
   </main>
 </template>
